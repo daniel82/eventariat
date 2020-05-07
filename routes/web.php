02045fd577e371136813a4dbe5ae77de8bd50c51
dmattke <daniel@mediebruket.no>
2020-05-07 16:36:56 +0200

    rec

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
   return redirect( "/dienstplan" );
 });
 
+Route::get('/booking',  "BookingComController@index");
+
 
 Route::middleware([ "auth", "locale"])->group(function ()
 {
