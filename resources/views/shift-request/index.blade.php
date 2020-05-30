7954edc616b0335eb074827d5f6d487802a52486
dmattke <daniel@mediebruket.no>
2020-05-30 08:24:56 +0200

    shift request, index, bugfix

--- resources/views/shift-request/index.blade.php
+++ resources/views/shift-request/index.blade.php
@@ -29,7 +29,12 @@
                 @endif
             </td>
             <td><span class="ev-shift-request__status status-{{ $item->status }}"><i class="fa fa-circle" aria-hidden="true"></i></span></td>
-            <td>{{ $item->user->first_name }} {{ $item->user->last_name }}</td>
+
+            <td>
+              @if ( is_object($item->user) )
+                {{ $item->user->first_name }} {{ $item->user->last_name }}
+              @endif
+            </td>
             <td>{{ $item->type_hr}}</td>
             <td>{{ formatDate($item->date_from, "d.m.Y")  }} - {{ formatDate($item->date_to, "d.m.Y") }}</td>
             <td class="ev-action-col">
